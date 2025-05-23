3eb8b0e3b8de72a0d69f16de1dacb6c8ded3b132
UDAYKOTESWARARAOCHAKRAPU <udaykoteswarao@gmail.com>
2025-05-23 14:49:29 +0530

    animation5

---
+++ animation5/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+     <div className="app-container">
+    <div className="background-animation">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
